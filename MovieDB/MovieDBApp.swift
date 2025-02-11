dec213f2364ebdd27256f16e560a5edbd0cb445d
Ahmed Eldyahi <ahmed.eldyahi@mdi.com.eg>
2025-02-11 12:39:58 +0200

    Initial Commit

---
+++ MovieDB/MovieDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDBApp.swift
+//  MovieDB
+//
+//  Created by Ahmed Eldyahi on 11/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct MovieDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
